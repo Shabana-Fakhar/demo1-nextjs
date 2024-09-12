a5c569a8a6aebe964eb78b635f57c95ea7a11daf
Shabana-Fakhar <shabanafakhar0861@gmail.com>
2024-09-12 20:14:42 +0500

    image

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
+import Hero from "@/components/hero"
 
-export default function Home(){
-  return(
-    <>
-    </>
-  );
+
+export default function Home() {
+return(
+<>
+   <Hero />
+</>
+);
+
+
 }
